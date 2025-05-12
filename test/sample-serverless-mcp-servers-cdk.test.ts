24e087a82062a8844a0bbe6a866c62b6fc0dc60d
huanshenyi <txy1226052@gmail.com>
2025-05-12 19:19:00 +0900

    Initial commit

---
+++ test/sample-serverless-mcp-servers-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SampleServerlessMcpServersCdk from '../lib/sample-serverless-mcp-servers-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sample-serverless-mcp-servers-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SampleServerlessMcpServersCdk.SampleServerlessMcpServersCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
